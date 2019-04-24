3b7447ae09f2fe3375a69102160c2df90c70684d
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-04-24 10:40:50 -0700

    we fixed async issue with loading images

--- js/app.js
+++ js/app.js
@@ -3,16 +3,9 @@
 console.log('app.js linked');
 
 // Global variables
-const imageArray = [];
 
-// This loads the image data
-function loadData(){
-  $.get('./data/page-1.json' , data => {
-    data.forEach( (element) => {
-      new Image(element.image_url, element.title, element.description, element.keyword, element.horns);
-    });
-  });
-}
+
+
 
 // An image constructor
 function Image(imageUrl, title, description, keyword, horns){
@@ -22,14 +15,41 @@ function Image(imageUrl, title, description, keyword, horns){
   this.keyword = keyword;
   this.horns = horns;
   imageArray.push(this);
+}
+const imageArray = [];
 
+// This loads the image data
+function loadData(){
+  $.get('./data/page-1.json' , data => {
+    data.forEach( (element) => {
+      new Image(element.image_url, element.title, element.description, element.keyword, element.horns);
+      return imageArray;
+    });
+  }).then(potato => 
+    potato.forEach(element => {
+      displayImages(element.imageUrl, element.title)
+    }))   
+}
+
+
+function displayImages(imageUrl, title){
+  console.log(imageUrl + ' | ' + title);
+  $('ul').append(`<li><img src="${imageUrl}" alt="${title}" /></li>`);
 }
 
 
 
-
 //Driver
+console.log("image array" + imageArray);
+
 loadData();
+
+// displayImages(imageArray[0].imageUrl, imageArray[0].title);
+
+// $.loadData().then(displayImages(imageArray[0].imageUrl, imageArray[0].title));
+
+
+
 
 
 
